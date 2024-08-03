9b2ebc507e4e04ab29d878be87e040b395bed1ea
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-08-03 23:54:26 +0700

    Initial Spring-GRPC Project.

---
+++ registry-service/src/main/java/dev/bengi/registryservice/RegistryServiceApplication.java
@@ -0,0 +1,17 @@
+package dev.bengi.registryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableDiscoveryClient
+public class RegistryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryServiceApplication.class, args);
+	}
+
+}
